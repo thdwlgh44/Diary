3c5a3cb5bb0b38dc7ca716cbd685787afd0ae267
jihoSong <thdwlgh44@naver.com>
2024-03-30 20:36:28 +0900

    state, prop

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,16 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+import { useState } from 'react';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+
   return (
     <>
-    <Button {...buttonProps}/>
-    <Button text={"카페"}/>
-    <Button text={"블로그"}>
-      <div>자식요소</div>
-    </Button>
+      <Bulb/>  
+      <Counter/>
     </>
+    
   );
 }
 
